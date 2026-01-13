4e7c9b5e490fb982b1d2eb9712948f1e88c91bea
Vince Kurtz <vjkurtz@gmail.com>
2026-01-13 15:44:43 -0600

    add implicit PD actuators for the left arm

--- simulate.py
+++ simulate.py
@@ -6,6 +6,7 @@
 #
 ##
 
+import numpy as np
 from pydrake.all import (
     StartMeshcat,
     DiagramBuilder,
@@ -17,11 +18,10 @@ from pydrake.all import (
     VisualizationConfig,
 )
 
-
 # Load the robot model.
 builder = DiagramBuilder()
 plant, scene_graph = AddMultibodyPlantSceneGraph(builder, time_step=0.005)
-Parser(plant).AddModels("urdf/stationary_ai.urdf")
+model_indices = Parser(plant).AddModels("urdf/stationary_ai.urdf")
 plant.Finalize()
 
 # Enable hydroelastic contact.
@@ -35,9 +35,23 @@ visualization_config = VisualizationConfig()
 visualization_config.publish_proximity = True
 ApplyVisualizationConfig(visualization_config, builder=builder, meshcat=meshcat)
 
-# Set up the simulator.
+# Build the system diagram
 diagram = builder.Build()
-simulator = Simulator(diagram)
+context = diagram.CreateDefaultContext()
+plant_context = plant.GetMyMutableContextFromRoot(context)
+
+# Fix joint targets for the implicit PD controllers in each joint
+model_instance_index = model_indices[0]
+nu = plant.num_actuators(model_instance_index)
+q_desired = np.zeros(nu)
+v_desired = np.zeros(nu)
+x_desired = np.concatenate([q_desired, v_desired])
+plant.get_desired_state_input_port(model_instance_index).FixValue(
+    plant_context, x_desired
+)
+
+# Set up the simulator.
+simulator = Simulator(diagram, context)
 simulator.set_target_realtime_rate(1.0)
 simulator.Initialize()
 
